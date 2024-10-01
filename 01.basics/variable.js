da8d0aaa352f69f16a26a4ea7933b02290d1c3fc
Prgupta25 <120772967+Prgupta25@users.noreply.github.com>
2024-10-01 21:12:18 +0000

    variable

---
+++ 01.basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 1222225
+let accountEmail = "prnav640@gmail.com"
+var accountpassword = "8516161"
+accountcity = "karnal"
+let accountstate
+
+accountEmail = "pranav940@gmail.com"
+accountpassword = "854191"
+accountcity = "chandigarh"
+
+
+console.table([accountId, accountEmail, accountpassword, accountcity, accountstate])
