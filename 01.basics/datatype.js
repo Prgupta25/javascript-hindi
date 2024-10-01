5d8459474c2426976e44ab68d1755278000ededd
Prgupta25 <120772967+Prgupta25@users.noreply.github.com>
2024-10-01 20:34:55 +0000

    datatype

---
+++ 01.basics/datatype.js
@@ -0,0 +1,24 @@
+"use strict"  // treat all js as new version
+
+//alert(3+3); // we are using nodejs not browser
+// for documentation tc39, mdn
+
+let name = "pranav"
+let age = 21
+let isLoggediin = false
+
+//Primitve datatype = 7
+// number => 2 ki power 53 tak range hoti hai
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = abhi value declared nhi hui hai
+// symbol = unique
+
+//object
+
+console.log(typeof age)
+
+console.log(typeof null) // iska type object hai
+console.log(typeof undefined)  // iska type undefined hai 
